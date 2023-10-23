aa34f41bafa3e3cb7e5eeaa98dc04fdc73dbc145
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-10-23 18:29:14 -0300

    feat: :sparkles: Add conditionals.js

--- Módulo 05 - JavaScript com jQuery/Aula 01 - Fundamentos do JavaScript I/Exercícios/conditionals.js
+++ Módulo 05 - JavaScript com jQuery/Aula 01 - Fundamentos do JavaScript I/Exercícios/conditionals.js
@@ -38,3 +38,40 @@ if (number > 0) {
 } else {
     alert("0")
 }
+// --------------------------------------------------------------------------------------------------------------
+// 03 - Show the sign
+// Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+// 1, if the value is greater than zero,
+// -1, if less than zero,
+// 0, if equals zero.
+// In this task we assume that the input is always a number.
+
+number = prompt("Write a number")
+
+if (number > 0) {
+    alert("1")
+} else if (number < 0){
+    alert("-1")
+} else {
+    alert("0")
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+// 04 - Rewrite 'if' into '?'
+// Rewrite this if using the conditional operator '?':
+
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Below';
+// } else {
+//   result = 'Over';
+// }
+
+a = Number(prompt("Digite o valor de A:"))
+b = Number(prompt("Digite o valor de B:"))
+
+let result = (a + b < 4) ? "Below" : "Over";
+alert(result)
