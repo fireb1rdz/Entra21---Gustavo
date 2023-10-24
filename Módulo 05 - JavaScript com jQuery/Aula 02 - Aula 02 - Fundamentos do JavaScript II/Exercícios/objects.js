f67013f90c2acc77ab2ae1a998ae6bc7e0288fc8
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-10-23 22:10:44 -0300

    feat: :sparkles: Fix index.html and objects.js

---
+++ Módulo 05 - JavaScript com jQuery/Aula 02 - Aula 02 - Fundamentos do JavaScript II/Exercícios/objects.js
@@ -0,0 +1,94 @@
+// 01 - Hello, object
+// Write the code, one line for each action:
+
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+user = {}
+user.name = "John"
+user.surname = "Smith"
+user.name = "Pete"
+delete user.name
+
+//-----------------------------------------------------------------
+
+// 02 - Check for emptiness
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+// Should work like that:
+
+// let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); // false
+
+let schedule = {name: "teste"}
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    } 
+    return true;
+}
+
+//-----------------------------------------------------------------
+
+// 03 - Sum object properties
+// We have an object storing salaries of our team:
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+//   Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+  
+//   If salaries is empty, then the result must be 0.
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const sumOfSalaries = obj => {
+    let acumulator = 0
+    for (let keys in salaries) {
+        acumulator += salaries[keys]
+} 
+    return acumulator
+}
+
+sumOfSalaries(salaries)
+
+//--------------------------------------------------------------------
+// 04 - Multiply numeric property values by 2
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+// For instance:
+
+// before the call
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+
+// multiplyNumeric(menu);
+
+// after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// P.S. Use typeof to check for a number here.
+
