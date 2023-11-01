8b78423ac6bce7a86c735e463a92a9f1701d9be5
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-11-01 18:27:13 -0300

    feat: :sparkles: add arrayMethods and arrays

---
+++ Módulo 05 - JavaScript com jQuery/Aula 02 - Aula 02 - Fundamentos do JavaScript II/Exercícios/arrayMethods.js
@@ -0,0 +1,10 @@
+// 01 - Translate border-left-width to borderLeftWidth
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+words = []
+
+const camelize = (word) => {
+    words
+}
--- Módulo 05 - JavaScript com jQuery/Aula 02 - Aula 02 - Fundamentos do JavaScript II/Exercícios/arrays.js
+++ Módulo 05 - JavaScript com jQuery/Aula 02 - Aula 02 - Fundamentos do JavaScript II/Exercícios/arrays.js
@@ -41,3 +41,46 @@ alert(arr.shift())
 arr.unshift("Rap", "Reggae")
 
 alert(arr)
+
+console.log(arr)
+
+// 03 - Calling in an array context
+// What is the result? Why?
+
+let arr2 = ["a", "b"];
+
+arr2.push(function() {
+  alert( this );
+});
+
+alert(arr2[2]()); // ?
+
+// 04 - Sum input numbers
+// Write the function sumInput() that:
+
+// Asks the user for values using prompt and stores the values in the array.
+// Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+// Calculates and returns the sum of array items.
+// P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+
+arr = []
+
+const sumInput = () => {
+    while (true) {
+        let value = prompt("Digite um número")
+        if (typeof(+value) !== "number" || value === "" || value === null) {
+            break;
+        } else {
+            arr.push(value)
+        }
+
+    }
+    accumulator = 0
+    for (num of arr) {
+        accumulator += Number(num)
+    }
+    alert(accumulator)
+
+}
+
+sumInput()
