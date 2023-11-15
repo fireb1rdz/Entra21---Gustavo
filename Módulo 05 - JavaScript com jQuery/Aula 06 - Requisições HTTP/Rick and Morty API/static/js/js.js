a2dbe87aa8c4ebfa2ac54933fe8f9d0684701544
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-11-15 15:35:52 -0300

    feat: :sparkles: feat and style

---
+++ Módulo 05 - JavaScript com jQuery/Aula 06 - Requisições HTTP/Rick and Morty API/static/js/js.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    const $form = $("#searchForm")
+    const $characterNameInput = $("#characterNameInput")
+    const $formSubmitButton = $("#formSubmitButton")
+    $characterNameInput.val("test")
+})
