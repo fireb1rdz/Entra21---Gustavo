4b14580b627360681e6236bcb1313dc06f80bbd7
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-11-15 11:11:43 -0300

    feat: :sparkles: feat: arrayMethods

--- Módulo 05 - JavaScript com jQuery/Aula 02 - Aula 02 - Fundamentos do JavaScript II/Exercícios/arrayMethods.js
+++ Módulo 05 - JavaScript com jQuery/Aula 02 - Aula 02 - Fundamentos do JavaScript II/Exercícios/arrayMethods.js
@@ -3,8 +3,12 @@
 
 // That is: removes all dashes, each word after dash becomes uppercased.
 
-words = []
-
 const camelize = (word) => {
-    words
-}
+    return word
+    .split('-')
+    .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join("")
+    };
+alert(camelize("border-left-width"))
