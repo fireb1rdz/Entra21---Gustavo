ba5f5265b48af564ccfd941e28af9a6f128d9685
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-11-15 17:57:34 -0300

    feat: :sparkles: feat: searching characters function

--- Módulo 05 - JavaScript com jQuery/Aula 06 - Requisições HTTP/Rick and Morty API/static/js/js.js
+++ Módulo 05 - JavaScript com jQuery/Aula 06 - Requisições HTTP/Rick and Morty API/static/js/js.js
@@ -1,6 +1,40 @@
 $(document).ready(() => {
+    const $cardsContainer = $("#cardsContainer")
     const $form = $("#searchForm")
     const $characterNameInput = $("#characterNameInput")
     const $formSubmitButton = $("#formSubmitButton")
-    $characterNameInput.val("test")
+    
+    $form.validate({
+        submitHandler: (form, event) => {
+            event.preventDefault();
+        }
+    })
+
+    let firstCharacter = true
+    let previousCharacter = ""
+
+    $form.on("submit", function () {
+        $cardsContainer.empty()
+        let currentCharacter = $characterNameInput.val()
+
+        fetch(`https://rickandmortyapi.com/api/character/?name=${$characterNameInput.val()}`)
+            .then(res => res.json())
+            .then(data => {
+                resultados = data.results
+                resultados.forEach(element => {
+                    $newCard = $("<div></div>").addClass("card mt-3 ms-0").css("width", "18rem")
+                    $cardImage = $(`<img src="${element.image}"></img>`).addClass("card-img-top")
+                    $cardBody = $("<div></div>").addClass("card-body")
+                    $cardTitle = $("<h5></h5>").addClass("card-title").text(element.name)
+                    $cardText = $("<p></p>").addClass("card-text").text(`Espécie: ${element.species} / Gênero: ${(element.gender === "Male") ? "masculino" : "feminino" }`)
+    
+                    $cardBody.append($cardTitle, $cardText)
+                    $newCard.append($cardImage, $cardBody)
+                    $cardsContainer.append($newCard)
+                });
+            })
+            .finally(() => {
+            })
+
+    })
 })
