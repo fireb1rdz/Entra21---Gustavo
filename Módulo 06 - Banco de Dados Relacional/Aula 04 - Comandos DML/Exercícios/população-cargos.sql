6e279f583cb72e2c8345aa08d5e8cfb23b65cc91
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-12-01 21:31:35 -0300

    feat: :sparkles: add: populating tables

---
+++ Módulo 06 - Banco de Dados Relacional/Aula 04 - Comandos DML/Exercícios/população-cargos.sql
@@ -0,0 +1,59 @@
+-- Inserir dados na tabela de cargos
+
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Recursos Humanos', 1);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de RH', 1);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Desenvolvedor Front-end', 2);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Desenvolvedor Back-end', 2);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Desenvolvimento de Software', 6);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Desenvolvedor de QA', 6);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Arquiteto de Software', 6);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Segurança da Informação', 2);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Administrador de Banco de Dados', 2);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Redes', 2);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Marketing', 3);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Mídias Sociais', 3);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Pesquisa de Mercado', 3);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Vendas', 4);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Representante de Vendas', 4);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Negociações', 4);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Coordenador de Vendas', 4);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Atendimento ao Cliente', 4);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Diretor Financeiro', 5);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista Financeiro', 5);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Contador', 5);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Orçamento', 5);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Cobrador', 5);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Logística', 7);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Cadeia de Suprimentos', 7);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Coordenador de Transporte', 7);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Armazenamento', 7);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Supervisor de Distribuição', 7);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Engenharia de Produtos', 8);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Designer de Produtos', 8);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Qualidade de Produtos', 8);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Supervisor de Atendimento ao Cliente', 9);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Suporte ao Cliente', 9);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Atendente', 9);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Compras', 10);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Compras', 10);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Coordenador de Suprimentos', 10);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de Controle de Qualidade', 11);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Analista de Garantia de Qualidade', 11);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Testes', 11);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Suporte Técnico', 12);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Técnico de Atendimento ao Cliente', 12);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Operador de Máquinas', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Engenheiro de produção', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Técnico em Manutenção Industrial', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Operador de Montagem', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Revisor', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Auxiliar de produção', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Supervisor de produção', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Gerente de produção', 13);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Instrutor de Treinamento', 14);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Especialista em Desenvolvimento de Conteúdo', 14);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Coordenador de Treinamento Corporativo', 14);
+INSERT INTO public.cargos (nome, departamento_id) VALUES ('Faxineira', 15);
+
+
+
---
+++ Módulo 06 - Banco de Dados Relacional/Aula 04 - Comandos DML/Exercícios/população-departamentos.sql
@@ -0,0 +1,17 @@
+-- Inserir nomes de departamentos aleatórios na tabela public.departamentos
+INSERT INTO public.departamentos (nome) VALUES ('Recursos Humanos');
+INSERT INTO public.departamentos (nome) VALUES ('TI - Tecnologia da Informação');
+INSERT INTO public.departamentos (nome) VALUES ('Marketing');
+INSERT INTO public.departamentos (nome) VALUES ('Vendas');
+INSERT INTO public.departamentos (nome) VALUES ('Finanças');
+INSERT INTO public.departamentos (nome) VALUES ('Desenvolvimento');
+INSERT INTO public.departamentos (nome) VALUES ('Logística');
+INSERT INTO public.departamentos (nome) VALUES ('Engenharia');
+INSERT INTO public.departamentos (nome) VALUES ('SAC');
+INSERT INTO public.departamentos (nome) VALUES ('Compras');
+INSERT INTO public.departamentos (nome) VALUES ('Qualidade');
+INSERT INTO public.departamentos (nome) VALUES ('Suporte Técnico');
+INSERT INTO public.departamentos (nome) VALUES ('Produção');
+INSERT INTO public.departamentos (nome) VALUES ('Treinamento');
+INSERT INTO public.departamentos (nome) VALUES ('Limpeza');
+SELECT * FROM departamentos
