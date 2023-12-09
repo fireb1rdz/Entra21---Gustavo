b7c95b260c79d613e45760703197b178dfc92e88
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-12-09 11:37:36 -0300

    feat: :sparkles: add function annotation into 5.py

--- Módulo 07 - Orientação à objetos/Exercicios/5.py
+++ Módulo 07 - Orientação à objetos/Exercicios/5.py
@@ -6,18 +6,59 @@ class Calculadora:
     """Essa classe representa uma calculadora"""
 
     def __init__(self, nome: str) -> None:
+        """Classe.
+        
+        Args:
+            nome (str): Nome da calculadora.
+        """
         self.nome = nome
 
     def soma(self, valor1, valor2):
+        """Realiza a soma dos valores informados como parâmetro.
+        
+        Args:
+            valor1 (int): Primeiro valor da soma.
+            valor2 (int): Segundo valor da soma.
+
+        Returns:
+            int: retorna o resultado da soma dos valores.
+        """
         return valor1 + valor2
     
     def subtracao(self, valor1, valor2):
+        """Realiza a subtração dos valores informados como parâmetro.
+        
+        Args:
+            valor1 (int): Primeiro valor da subtração.
+            valor2 (int): Segundo valor da subtração.
+
+        Returns:
+            int: retorna o resultado da subtração dos valores.
+        """
         return valor1 - valor2
     
     def multiplicacao(self, valor1, valor2):
+        """Realiza a multiplicação dos valores informados como parâmetro.
+        
+        Args:
+            valor1 (int): Primeiro valor da multiplicação.
+            valor2 (int): Segundo valor da multiplicação.
+
+        Returns:
+            int: retorna o resultado da multiplicação dos valores.
+        """
         return valor1 * valor2
     
     def divisao(self, valor1, valor2):
+        """Realiza a divisão dos valores informados como parâmetro.
+        
+        Args:
+            valor1 (int): Dividendo.
+            valor2 (int): Divisor.
+
+        Returns:
+            int: retorna o resultado da divisão dos valores.
+        """
         return valor1 / valor2
 
 if __name__ == "__main__":
