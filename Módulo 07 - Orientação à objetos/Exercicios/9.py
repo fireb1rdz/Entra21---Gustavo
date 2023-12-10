b9582c994bbfccf0ff5d1ba1ecb1f897f6bddb82
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-12-10 19:54:33 -0300

    feat: :sparkles: refactor 9.py

--- Módulo 07 - Orientação à objetos/Exercicios/9.py
+++ Módulo 07 - Orientação à objetos/Exercicios/9.py
@@ -9,23 +9,33 @@ Implemente um método estático chamado get_total_pessoas() que retorna o valor 
 
 class Pessoa:
 
-    total_pessoas = 0
+    # Atributo estático privado da classe.
+    __total_pessoas = 0
 
     def __init__(self, nome: str, idade: int) -> None:
-        """Essa classe representa uma pessoa"""
+        """Essa classe representa uma pessoa
+        
+        Args:
+            nome (str): Nome da pessoa
+            idade (int): Idade da pessoa
+        """
         self.nome = nome
         self.idade = idade
-        Pessoa.total_pessoas += 1
-    
+        Pessoa.__total_pessoas += 1
+
+    def __str__(self) -> str:
+        return f"Nome: {self.nome} | Idade: {self.idade}"
+        
     def get_total_pessoas() -> None:
         """Retorna a quantidade total de instâncias da classe Pessoa
         
         Returns:
             total_pessoas (int): Quantidade total de instâncias da classe.
         """
-        return Pessoa.total_pessoas
+        return Pessoa.__total_pessoas
     
 if __name__ == "__main__":
     gustavo = Pessoa("Gustavo", 18)
-    gustavo = Pessoa("Sabrina", 23)
+    sabrina = Pessoa("Sabrina", 23)
     print(Pessoa.get_total_pessoas())
+    print(gustavo, sabrina)
