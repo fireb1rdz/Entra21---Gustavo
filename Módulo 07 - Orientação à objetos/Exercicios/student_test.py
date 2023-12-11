abea6c536ad292f13b0baf448085d02905bc948e
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-12-11 19:04:52 -0300

    feat: :sparkles: add venv

--- Módulo 07 - Orientação à objetos/Exercicios/student_test.py
+++ Módulo 07 - Orientação à objetos/Exercicios/student_test.py
@@ -3,8 +3,8 @@ import pytest
 from student import Aluno
 
 def test_student_creation():
-    student = Student("123123", "Gustavo", [10, 10, 10])
+    student = Aluno("123123", "Gustavo", [10, 10, 10])
 
-    assert student.name == "Gustavo"
-    assert student.grades == [10, 10 ,10]
+    assert student.nome == "Gustavo"
+    assert student.notas == [10, 10 ,10]
     assert student.numero_matricula == "123123"
