dd02d69a820561b9354a1a27ec8d0f8e09455b1a
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-12-11 18:38:40 -0300

    feat: :sparkles: 2.py

--- Módulo 07 - Orientação à objetos/Exercicios/2.py
+++ Módulo 07 - Orientação à objetos/Exercicios/2.py
@@ -46,13 +46,12 @@ class Aluno:
 
         if self.get_media() <= 4:
             return f"Reprovado!"
-        elif self.get_media() <= 6:
+        if self.get_media() <= 6:
             return f"Recuperação!"
-        elif self.get_media() <= 10:
-            return f"Aprovado!"
+        return f"Aprovado!"
     
 if __name__ == "__main__":
-    gustavo = Aluno(123123, "Gustavo", [10, 2, 2])
+    gustavo = Aluno(123123, "Gustavo", [10, 2, 8])
     print(f"""
 Matrícula: {gustavo.numero_matricula}
 Nome: {gustavo.nome} 
